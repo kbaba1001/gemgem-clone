9f65efcd0047ff76752fde952bb7cc183ee28331
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-07-11 15:43:45 +0900

    cosmetics

--- test/controllers/things_controller_test.rb
+++ test/controllers/things_controller_test.rb
@@ -4,7 +4,7 @@ class ThingsControllerTest < ActionDispatch::IntegrationTest
   test '#new' do
     visit new_thing_path
 
-    assert { page.has_selector('form #thing_name') == true }
+    assert { page.has_selector?('form #thing_name') == true }
 
     # assert_select "form #thing_name"
     # assert_select "form #thing_name.readonly", false
