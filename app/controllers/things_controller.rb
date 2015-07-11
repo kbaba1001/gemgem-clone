04ca2bf400f7f58f1adb0598b998f326b4120e61
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-07-11 16:23:03 +0900

    define Thing#create

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,14 +1,17 @@
 class ThingsController < ApplicationController
   def new
-    @form = Thing.new
+    form Thing::Create
   end
 
   def create
+    run Thing::Create do |op|
+      redirect_to op.model and return
+    end
 
+    render action: :new
   end
 
   def show
-
   end
 
   def edit
