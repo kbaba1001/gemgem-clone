48b306ac13829a6c4da993d3cfd37ddfa7d4bcb0
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-08-11 22:51:11 +0900

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811134601) do
+ActiveRecord::Schema.define(version: 20150811135005) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20150811134601) do
     t.text     "image_meta_data"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "confirmation_token"
+    t.text     "image_meta_data"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
 end
